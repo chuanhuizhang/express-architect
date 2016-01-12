da2f85b58fb66b201e7fb82cb1ca671a73b11a91
cz-Michael <zchning@gmail.com>
2016-01-12 16:23:32 -0500

    hash the password before save

--- mod/user/userModel.js
+++ mod/user/userModel.js
@@ -1,3 +1,7 @@
+'use strict'
+
+var bcrypt = require('bcrypt-nodejs');
+
 module.exports = function(db) {
     var schema = {
         email: { type: String, unique: true, required: true },
@@ -8,6 +12,25 @@ module.exports = function(db) {
     }
 
     var UserSchema = db.schema(schema);
+
+    // Execute before each user.save() call
+    UserSchema.pre('save', function(callback) {
+        var user = this;
+
+        // Break out if the password hasn't changed
+        if (!user.isModified('password')) return callback();
+
+        // Password changed so we need to hash it
+        bcrypt.genSalt(5, function(err, salt) {
+            if (err) return callback(err);
+
+            bcrypt.hash(user.password, salt, null, function(err, hash) {
+                if (err) return callback(err);
+                user.password = hash;
+                callback();
+            });
+        });
+    });
 
     return {
         getInstance: function() {
